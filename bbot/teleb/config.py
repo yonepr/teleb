256210b1d577937d12c04db257992e9832b9f194
yonepr <130835843+yonepr@users.noreply.github.com>
2023-05-05 15:52:15 +0300

    Add files via upload

---
+++ bbot/teleb/config.py
@@ -0,0 +1,3 @@
+api = '5360966255:AAF2XzAxP38qCtEna1-ROio1bHkyKysjvVU'
+PWD_DB = ''
+USER_DB = ''
---
+++ bbot/teleb/main.py
@@ -0,0 +1,54 @@
+import telebot
+from config import api
+from telebot import types
+
+bot = telebot.TeleBot(f"{api}")
+
+bot.delete_webhook()
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	btn1 = types.KeyboardButton("👋 Авторизация")
+	btn2 = types.KeyboardButton("❓ Помощь")
+	markup.add(btn1, btn2)
+	bot.send_message(message.chat.id, "Привет, {0.first_name}! 🚗 \nДанный бот позволяет открывать шлагбаум 😎, находящийся на территории КБО для сотрудников \"ТКС Саров 📞\"".format(message.from_user), reply_markup=markup)
+
+	@bot.message_handler(content_types='text')
+	def reg(message):
+		if message.text=="👋 Авторизация":
+			markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+			phone_number = types.KeyboardButton("Отправить контакт", request_contact=True)
+			btn2 = types.KeyboardButton("❓ Помощь")
+			markup.add(phone_number, btn2)
+			bot.send_message(message.chat.id,"Отправьте свой контакт для авторизации, пожалуйста. (Нажмите на кнопку)",reply_markup=markup)
+
+		elif message.text=="❓ Помощь":
+			bot.send_message(message.chat.id, "Для использования данного телеграм-бота пройдите этап авторизации, запросите пароль у Вашего администратора.\nПосле того как Вы авторизируйтесь, Вам откроются функции по открытию шлагбаума")
+
+		@bot.message_handler(content_types=['contact'])
+		def contact(message):
+			if message.contact is not None:
+				print(message.contact.phone_number)
+
+
+
+@bot.message_handler(commands=['admin'])
+def adm(message):
+	a = telebot.types.ReplyKeyboardRemove()
+	bot.send_message(message.chat.id, text="Здравствуйте, {0.first_name}!\nВы перешли в панель администратора.\nДля того что бы удостовериться, что это вы, пожалуйста введите пароль.".format(message.from_user),reply_markup=a)
+	@bot.message_handler(content_types='text')
+	def nxtadm(message):
+		if message.text == "12345678":
+			markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+			add = types.KeyboardButton("Добавить")
+			reset = types.KeyboardButton("Изменить")
+			delete = types.KeyboardButton("Удалить")
+			request = types.KeyboardButton("Запросить таблицу")
+			opppen = types.KeyboardButton("Открыть шлагбаум")
+			markup.add(add,reset,delete,request,opppen)
+			bot.send_message(message.chat.id,"Вы полноценно зашли в панель администратора,вам открылись все возможности.",reply_markup=markup)
+		elif message.text != "":
+			bot.send_message(message.chat.id,"Неверный пароль, проверьте, что вы ввели его правильно.\nПопробуйте ещё раз.Или введите '/start'.")
+
+bot.polling(none_stop=True)
